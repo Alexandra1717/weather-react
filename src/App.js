099cd3c5c68dcb5fb818b6b3862b47a230c91587
Alexandra1717 <66381677+Alexandra1717@users.noreply.github.com>
2020-09-06 23:05:36 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ export default function App() {
           <Forecast />
         </div>
       </div>
-      <small> <a href="https://github.com/Alexandra1717/weather-app-original" target="blank">Open source code </a> by Alexandra Negrea</small>
+      <small> <a href="https://github.com/Alexandra1717/weather-react" target="blank">Open source code </a> by Alexandra Negrea</small>
     </div>
   );
 }
